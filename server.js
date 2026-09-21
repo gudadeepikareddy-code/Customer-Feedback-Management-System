eb318731da594af59612627cf39b17df02eb65ea
GUDA DEEPIKA REDDY <gudadeepikareddy@gmail.com>
2026-09-21 22:01:42 +0530

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const dbConfig = {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 
 // HOME
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
